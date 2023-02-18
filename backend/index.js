8b4e2c46d887e63a721ab488c485447113934bc8
Julia <julia.sarandi@gmail.com>
2023-02-17 22:25:33 -0800

    MEET-1 Liquibase initial commit.

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,19 @@
 const envVar = require('./env-vars');
 const express = require('express');
 const { log } = require('./tools/logger');
+const nodeLiquibase = require('node-liquibase');
+
+// Liquibase configuration
+const myConfig = {
+  ...nodeLiquibase.POSTGRESQL_DEFAULT_CONFIG,
+  changeLogFile: './db/changelog.xml',
+  url: `jdbc:postgresql://localhost:${envVar.DB_PORT}/${envVar.DB_NAME}`,
+  username: envVar.DB_USERNAME,
+  password: envVar.DB_PASSWORD
+};
+const instTs = new nodeLiquibase.Liquibase(myConfig);
+instTs.status();
+instTs.update(null);
 
 // Basic express setup:
 const app = express();
