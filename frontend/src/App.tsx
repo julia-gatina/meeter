f3a17e2a2b0d0b640c442e046639a2c2c36f46ae
Julia <julia.sarandi@gmail.com>
2023-02-18 15:11:29 -0800

    MEET-5 Project cleanup.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { createElement as e } from "react";
+import "./App.scss";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return e("div", { className: "container" }, [
+    e("h1", { className: "container" }, "Some text"),
+    e("button", { className: "bg-danger" }, "Click Me"),
+  ]);
+  return <h1>Hello React !!!</h1>;
 }
 
 export default App;
